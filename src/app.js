f371b733a96066b260e043e4ecfa315e809d8676
Thyago-ES <thyagorafael57@gmail.com>
2024-11-04 15:57:21 -0300

    feat: add Comment routes on app configuration

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const userRoutes = require("./app/routes/userRoutes");
 const postRoutes = require("./app/routes/postRoutes");
+const commentRoutes = require("./app/routes/commentRoutes");
 
 require("./database");
 
@@ -19,6 +20,7 @@ class App {
 	routes() {
 		this.server.use("/users", userRoutes);
 		this.server.use("/users/:userId/posts", postRoutes);
+		this.server.use("/posts/:postId/comments", commentRoutes);
 	}
 }
 
